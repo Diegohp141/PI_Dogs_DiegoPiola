35b5f3faf87779c3cb90dec38c5ef1c2225523a5
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-02-22 17:59:59 -0300

    test routes working

--- PI-Dogs-main/api/src/routes/index.js
+++ PI-Dogs-main/api/src/routes/index.js
@@ -1,12 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
+const dbDogs = require("./dbDogs.js");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use("/dbDogs", dbDogs);
 
+router.get("/", async (req, res) => {
+  res.send("hola desde el /");
+});
 
 module.exports = router;
