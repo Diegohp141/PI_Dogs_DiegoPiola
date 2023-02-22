e35cdd3c14f784eba73a2bba81d60b26f88e780f
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-02-22 19:16:35 -0300

    Second route file created and working

--- PI-Dogs-main/api/src/routes/index.js
+++ PI-Dogs-main/api/src/routes/index.js
@@ -4,10 +4,12 @@ const { Router } = require("express");
 
 const router = Router();
 const dbDogs = require("./dbDogs.js");
+const apiRoutes = require("./apiDogs.js");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use("/dbDogs", dbDogs);
+router.use("/apiRoute", apiRoutes);
 
 router.get("/", async (req, res) => {
   res.send("hola desde el /");
