b9465581ec031f7b343c7348a63d63109d26ca44
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-02-22 18:17:01 -0300

    Creation of tables when starting the server and test routes working correctly

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -1,12 +1,50 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUID,
+        allowNull: false,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      height: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      life_span: {
+        type: DataTypes.STRING,
+      },
+
+      img: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          isUrl: true,
+        },
+      },
+      createdByDB: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
