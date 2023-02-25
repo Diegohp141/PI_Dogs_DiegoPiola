1fe6047eb3b949da1072781bb5de8bd244424a41
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-02-25 09:41:37 -0300

    Model Dog update

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -31,7 +31,7 @@ module.exports = (sequelize) => {
       },
 
       img: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
         validate: {
           isUrl: true,
