35e61f582bf1c8d754daf9db64030d3a69b9ec34
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-02-27 18:53:02 -0300

    general configs

--- PI-Dogs-main/client/src/components/landingPage/LandingPage.jsx
+++ PI-Dogs-main/client/src/components/landingPage/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from "./LandingPage.module.css";
 import { Link } from "react-router-dom";
 
 export default function Landingpage() {
   return (
-    <div>
+    <div className={style.divContainer}>
       <h1>Welcome to my dog ​​api!</h1>
       <h3>Prepare for cuteness</h3>
       <Link to="/home">
