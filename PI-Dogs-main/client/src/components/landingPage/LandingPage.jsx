cb734ec90c69d28e427801993f5f60ab4ea6a960
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-02-27 21:39:34 -0300

    new push

--- PI-Dogs-main/client/src/components/landingPage/LandingPage.jsx
+++ PI-Dogs-main/client/src/components/landingPage/LandingPage.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import style from "./LandingPage.module.css";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavLink = styled(Link)`
+  font-size: 8vh;
+  width: 10%;
+  border: 2px solid rgb(20, 94, 20);
+  border-radius: 10px;
+  color: white;
+  text-decoration: none;
+  padding: 0px 10px 0px 10px;
+  margin-bottom: 250px;
+  background-color: rgba(20, 94, 20, 0.8);
+  &:hover {
+    color: rgb(201, 184, 184);
+    cursor: pointer;
+  }
+`;
 
 export default function Landingpage() {
   return (
     <div className={style.divContainer}>
-      <h1>Welcome to my dog ​​api!</h1>
-      <h3>Prepare for cuteness</h3>
-      <Link to="/home">
-        <button>Enter</button>
-      </Link>
+      <h1 className={style.title}>Welcome to my dog ​​api!</h1>
+      <h3 className={style.subTitle}>Prepare for cuteness</h3>
+      <NavLink to="/home">Enter</NavLink>
     </div>
   );
 }
